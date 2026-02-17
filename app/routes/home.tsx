ea3a34fc798f2733f7247b209ebef7645cabfc1f
rohit1verma <vermarohit7780@gmail.com>
2026-02-17 17:52:32 +0530

    implement puter workers, creation of the 3D floorplan and the creation of the other objects

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,143 +1,158 @@
-import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
-import Navbar from "../../components/Navbar";
 import type { Route } from "./+types/home";
-import Button from "components/ui/Button";
-import Upload from "components/Upload";
-import { useNavigate } from "react-router";
-import { useState } from "react";
-import { createProject } from "lib/puter.action";
+import Navbar from "../../components/Navbar";
+import {ArrowRight, ArrowUpRight, Clock, Layers} from "lucide-react";
+import Button from "../../components/ui/Button";
+import Upload from "../../components/Upload";
+import {useNavigate} from "react-router";
+import {useEffect, useRef, useState} from "react";
+import {createProject, getProjects} from "../../lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Roomify" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Welcome to Roomify!" },
   ];
 }
 
 export default function Home() {
+    const navigate = useNavigate();
+    const [projects, setProjects] = useState<DesignItem[]>([]);
+    const isCreatingProjectRef = useRef(false);
 
-  const navigate = useNavigate();
-  const [projects, setProjects] = useState<DesignItem[]>([])
+    const handleUploadComplete = async (base64Image: string) => {
+        try {
 
-  const handleUploadComplete = async (base64Image:string) => {
-    const newId = Date.now().toString();
+            if(isCreatingProjectRef.current) return false;
+            isCreatingProjectRef.current = true;
+            const newId = Date.now().toString();
+            const name = `Residence ${newId}`;
 
-    const name = `Residence ${newId}`;
+            const newItem = {
+                id: newId, name, sourceImage: base64Image,
+                renderedImage: undefined,
+                timestamp: Date.now()
+            }
 
-    const newItem = {
-      id:newId, name, sourceImage: base64Image, renderedImage: undefined,
-      timestamp: Date.now()
+            const saved = await createProject({ item: newItem, visibility: 'private' });
+
+            if(!saved) {
+                console.error("Failed to create project");
+                return false;
+            }
+
+            setProjects((prev) => [saved, ...prev]);
+
+            navigate(`/visualizer/${newId}`, {
+                state: {
+                    initialImage: saved.sourceImage,
+                    initialRendered: saved.renderedImage || null,
+                    name
+                }
+            });
+
+            return true;
+        } finally {
+            isCreatingProjectRef.current = false;
+        }
     }
-    const saved = await createProject({item: newItem, visibility:'private'});
 
-    if(!saved){
-      console.error("Failed to create project");
-      return false;
-    }
-    setProjects((prev) => [saved, ...prev]);
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const items = await getProjects();
 
-    navigate(`/visualizer/${newId}`, {
-      state:{
-        initialImage: saved.sourceImage,
-        initialRendered: saved.renderedImage || null,
-        name
-      }
-    })
-    return true;
-  }
+            setProjects(items)
+        }
+
+        fetchProjects();
+    }, []);
+
   return (
-    <div className='home'>
-      <Navbar />
+      <div className="home">
+          <Navbar />
 
-      <section className='hero'>
-        <div className='announce'>
-          <div className='dot'>
-            <div className='pulse'></div>
-          </div>
-
-          <p>Introducing Roomify 2.0</p>
-        </div>
-
-        <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
-        <p className='subtitle'>
-          Roomify is an AI-first design environment that helps you to visualize,
-          render, and ship architectural projects faster than ever.
-        </p>
-
-        <div className='actions'>
-          <a href='#upload' className='cta'>
-            Start Building <ArrowRight className='icon' />
-          </a>
-
-          <Button variant='outline' size='lg' className='demo'>
-            Watch Demo
-          </Button>
-        </div>
-
-        <div id='upload' className='upload-shell'>
-          <div className='grid-overlay' />
-
-          <div className='upload-card'>
-            <div className='upload-head'>
-              <div className='upload-icon'>
-                <Layers className='icon' />
-              </div>
-
-              <h3>Upload your floor plan</h3>
-              <p>Supports JPG, JPEG, PNG, WebP formats up to 10MB</p>
-            </div>
-
-            <Upload onComplete={handleUploadComplete} />
-          </div>
-        </div>
-      </section>
-
-      <section className='projects'>
-        <div className='section-inner'>
-          <div className='section-head'>
-            <div className='copy'>
-              <h2>Projects</h2>
-              <p>
-                Your latest work and shared community projects, all in one
-                place.
-              </p>
-            </div>
-          </div>
-
-          <div className='projects-grid'>
-            {projects.map(({id,name,renderedImage, sourceImage, timestamp})=> (
-
-            
-            <div key={id} className='project-card group'>
-              <div className='preview'>
-                <img
-                  src={renderedImage || sourceImage}
-                  alt='Project'
-                />
-                <div className='badge'>
-                  <span>Community</span>
-                </div>
-              </div>
-
-              <div className='card-body'>
-                <div>
-                  <h3>{name}</h3>
-
-                  <div className='meta'>
-                    <Clock size={12} />
-                    <span>{new Date(timestamp).toLocaleDateString()}</span>
-                    <span>By Rohit</span>
+          <section className="hero">
+              <div className="announce">
+                  <div className="dot">
+                      <div className="pulse"></div>
                   </div>
-                </div>
-                <div className='arrow'>
-                  <ArrowUpRight size={18} />
-                </div>
+
+                  <p>Introducing Roomify 2.0</p>
               </div>
-            </div>
-            ))}
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+
+              <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
+
+              <p className="subtitle">
+                  Roomify is an AI-first design environment that helps you visualize, render, and ship architectural projects faster  than ever.
+              </p>
+
+              <div className="actions">
+                  <a href="#upload" className="cta">
+                      Start Building <ArrowRight className="icon" />
+                  </a>
+
+                  <Button variant="outline" size="lg" className="demo">
+                      Watch Demo
+                  </Button>
+              </div>
+
+              <div id="upload" className="upload-shell">
+                <div className="grid-overlay" />
+
+                  <div className="upload-card">
+                      <div className="upload-head">
+                          <div className="upload-icon">
+                              <Layers className="icon" />
+                          </div>
+
+                          <h3>Upload your floor plan</h3>
+                          <p>Supports JPG, JPEG, PNG, WEBP formats up to 50MB</p>
+                      </div>
+
+                      <Upload onComplete={handleUploadComplete} />
+                  </div>
+              </div>
+          </section>
+
+          <section className="projects">
+              <div className="section-inner">
+                  <div className="section-head">
+                      <div className="copy">
+                          <h2>Projects</h2>
+                          <p>Your latest work and shared community projects, all in one place.</p>
+                      </div>
+                  </div>
+
+                  <div className="projects-grid">
+                      {projects.map(({id, name, renderedImage, sourceImage, timestamp}) => (
+                          <div key={id} className="project-card group" onClick={() => navigate(`/visualizer/${id}`)}>
+                              <div className="preview">
+                                  <img  src={renderedImage || sourceImage} alt="Project"
+                                  />
+
+                                  <div className="badge">
+                                      <span>Community</span>
+                                  </div>
+                              </div>
+
+                              <div className="card-body">
+                                  <div>
+                                      <h3>{name}</h3>
+
+                                      <div className="meta">
+                                          <Clock size={12} />
+                                          <span>{new Date(timestamp).toLocaleDateString()}</span>
+                                          <span>By JS Mastery</span>
+                                      </div>
+                                  </div>
+                                  <div className="arrow">
+                                      <ArrowUpRight size={18} />
+                                  </div>
+                              </div>
+                          </div>
+                      ))}
+                  </div>
+              </div>
+          </section>
+      </div>
+  )
 }
